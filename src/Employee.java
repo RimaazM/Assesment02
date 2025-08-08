041dc56217e03619f3d4cccbe059804b201b64c5
RimaazM <mrimaaz26@gmail.com>
2025-08-08 15:12:44 +1100

    created variables for employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,9 @@
-public class Employee {
+public class Employee extends Person {
+    private String employeeId;
+    private String position;
+
+    public Employee() { super(); }
+    public Employee(String name, int age, String contact, String employeeId, String position) { ... }
+
+
 }
