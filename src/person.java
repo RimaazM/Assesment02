62b9bcf503843aa52f028d51474b1d13818f499e
RimaazM <mrimaaz26@gmail.com>
2025-08-08 14:55:07 +1100

    added the variables

--- src/person.java
+++ src/person.java
@@ -1,2 +1,9 @@
-public class person {
+public abstract class person {
+    private string name;
+    private int age;
+    private string contactNumber;
+
+    public person () {}
+    public person(string name, int age, string contactNumber) { ... }
+
 }
