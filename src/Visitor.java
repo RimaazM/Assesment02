21aba04da1912b5f36adc76701dbb7571476cf63
RimaazM <mrimaaz26@gmail.com>
2025-08-08 15:15:45 +1100

    representing theme park visitors

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,2 +1,9 @@
-public class Visitor {
+public class Visitor extends Person {
+    private String visitorId;
+    private String ticketType;
+
+    public Visitor() { super(); }
+    public Visitor(String name, int age, String contact, String visitorId, String ticketType) { ... }
+
+
 }
